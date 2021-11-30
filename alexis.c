02cfc7a79b42042dc3f3b509fcd572fba84a3d7c
alexismarieece <alexis.marie@edu.ece.fr>
2021-12-01 00:15:33 +0100

    Update alexis.c
    Deux fonctions rajoutées dans alexis, celle du placement de la barriere et le produit brut de la fonction de dpelacement.

--- alexis.c
+++ alexis.c
@@ -53,3 +53,51 @@ int main()
     }
     return 0;
 }
+
+void placerBarriere(char ligne, int colonne)
+{
+    if((ligne%2!=0)&&(colonne%2!=0)&&(ligne<17)&&(colonne!=17))
+    {
+        if (plateau[ligne][colonne]!=0)
+        {
+            plateau[ligne][colonne]=0;
+        }
+        else
+        {
+            printf("erreur il y a deja une barriere ! ");
+        }
+    }
+    else
+    {
+        printf("c est l endroit ou on place les pions pas de barriere ! ");
+    }
+}
+
+void allerCase(char ligne, int colonne)
+{
+    if((ligne%2==0)&&(colonne%2==0)&&(ligne<17)&&(colonne<17))
+    {
+        if()//La case entre celle ou il est et celle ou il veut aller il y a une barriere... il faut une fonction case à l'instant t.
+        {
+            printf("impossible de se deplacer il y a une barriere et tu n es pas un fantome voyons...");
+        }
+        if(plateau[ligne][colonne]==pion)//creer une fonction booleenne qui renvoie true si pion correspond à un carac de pion, false sinon.
+        {
+            printf("il y a deja un pion a cet endroit, choisir de sauter le pion ou d annuler");
+            int i=1;
+            while (i>0)
+            {
+                allerCase(ligne1,colonne1);
+                if((abs(ligne1-ligne)>1)||(abs(colonne1-colonne)>1)||((abs(ligne1-ligne)+abs(colonne1-colonne)>1)))
+                {
+                    printf("saisie incorrecte la case est trop eloignee");
+                }
+                else
+                {
+                    plateau[ligne][colonne]=pion; //le pion du joueur concerné
+                }
+            }
+        }
+        
+    }
+}
