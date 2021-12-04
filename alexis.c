fcfeebf4b203aaa6ebfc18cf0537b5504a83e4a3
alexismarieece <alexis.marie@edu.ece.fr>
2021-12-04 03:35:57 +0100

    Update alexis.c

--- alexis.c
+++ alexis.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<math.h>
 
+typedef struct joueur
+{
+    char nom[20];
+    int pion;
+    int positionI;
+    int positionJ;
+    int score;
+    int barriere_posees;
+    int nbCoupAnnule;
+}t_joueur;
 
 void initialiserPlateau (int plateau[17][17])
 {
@@ -15,31 +26,8 @@ void initialiserPlateau (int plateau[17][17])
     }
 }
 
-
-void deplacerPion(int i,int j,int i1,int j1,int plateau[17][17])
+void afficher(int plateau[17][17])//nécessaire ou pas ?
 {
-    plateau[i1][j1]=1;
-    plateau[i][j]=0;
-}
-
-void afficher(int plateau[17][17])
-{
-    int i,j;
-    for (i=0;i<17;i++)
-    {
-        for (j=0;j<17;j++)
-        {
-            if (plateau[i][j]==1)
-                {
-                    printf("%c",'P');
-                }
-        }
-    }
-}
-
-int main()
-{
-    int plateau[17][17];
     //Demander la taille
     initialiserPlateau(plateau);
     int i,j;
@@ -51,55 +39,89 @@ int main()
         }
         printf("\n");
     }
-    return 0;
 }
 
-void placerBarriere(char ligne, int colonne, int plateau[17][17])
+/*void saisieEmplacementJoueur(t_joueur *Ptjoueur)//nouveau
 {
-    if((ligne%2!=0)&&(colonne%2!=0)&&(ligne<17)&&(colonne!=17))
-    {
-        if (plateau[ligne][colonne]!=1)
-        {
-            plateau[ligne][colonne]=1;
-        }
-        else
-        {
-            printf("erreur il y a deja une barriere ! ");
-        }
-    }
-    else
-    {
-        printf("c est l endroit ou on place les pions pas de barriere ! ");
-    }
-}
+    printf("saisir la position ligne du joueur");
+    scanf("%d",&(Ptjoueur->positionI));
+    printf("saisir la position colonne du joueur");
+    scanf("%d",&(Ptjoueur->positionJ));
+}*/
 
-void allerCase(char ligne, int colonne, int plateau[17][17])
+void deplacerPion(int i1,int j1,int plateau[17][17])
 {
-    if((ligne%2==0)&&(colonne%2==0)&&(ligne<17)&&(colonne<17))
-    {
-        if(ligne!=NULL)//La case entre celle ou il est et celle ou il veut aller il y a une barriere... il faut une fonction case à l'instant t.
-        {
-            printf("impossible de se deplacer il y a une barriere et tu n es pas un fantome voyons...");
-        }
-        if(plateau[ligne][colonne]!=0)//creer une fonction booleenne qui renvoie true si pion correspond à un carac de pion, false sinon.
-        {
-            printf("il y a deja un pion a cet endroit, choisir de sauter le pion ou d annuler");
-            int i=1;
-            while (i>0)
-            {
-                allerCase(ligne1,colonne1,plateau);
-                if((abs(ligne1-ligne)>2)||(abs(colonne1-colonne)>2)||((abs(ligne1-ligne)+abs(colonne1-colonne)>2)))
-                {
-                    printf("saisie incorrecte la case est trop eloignee");
-                }
-                else
-                {
-                    plateau[ligne][colonne]=pion; //le pion du joueur concerné
-                }
-            }
-        }
-        
-    }
+    int i,j;
+    t_joueur joueur;
+    i=joueur.positionI;
+    j=joueur.positionJ;
+    plateau[i1][j1]=joueur.pion;
+    plateau[i][j]=0;
 }
-// A voir ensemble
 
+int verificationDeplacement(int i1,int j1,int plateau[17][17])
+{
+    t_joueur joueur;
+    int i,j;
+    i=joueur.positionI;
+    j=joueur.positionJ;
+    if ((i1%2==0)||(j1%2==0))
+    {
+        printf("le pion est dans la case barriere");
+        return(1);//retourne faux si le pion est dans une case barriere
+    }
+    if (i!=i1)
+    {
+        if (abs(i-i1)>2)
+        {
+            printf("Le pion est deplace trop loin");
+            return(1);//le déplacement vertical est tro important
+        }
+    }
+    if (j!=j1)
+    {
+        if (abs(j-j1)>2)
+        {
+            printf("Le pion est deplace trop loin");
+            return(1);//le deplacement horizontal est trop important
+        }
+    }
+    if ((i!=i1)&&(j!=j1))
+    {
+        printf("un pion ne peut pas se deplacer en diagonale");
+        return(1);//Le pion ne se deplace pas en diagonale
+    }
+    if(verifierBarriere(i,j,i1,j1,plateau)==1)
+    {
+        printf("une barriere empeche le deplacement");
+        return(1);
+    }
+    return(0);//Si les conditions non valables ne sont pas verifiees, alors on valide la faisabilite
+}
+
+int verifierBarriere(int i,int j,int i1,int j1,int plateau[17][17])// va dans lee programme verificationDeplacement
+{
+    if (i!=i1)
+    {
+        if (plateau[(i+i1)/2]==1)//S il y a une barriere entre les deux cases
+        {
+            printf("il y a une barriere\n");
+            return(1);
+        }
+    }
+    if (j!=j1)
+    {
+        if (plateau[(j+j1)/2]==1)//S il y a une barriere entre les deux cases
+        {
+            printf("il y a une barriere\n");
+            return(1);
+        }
+    }
+    return(0);
+}
+
+int pionDansCase(int i1,int j1,int plateau[17][17])
+{
+    //si il y a un pion dans la case, il faut re saisir un endroit ou se deplacer et tout re verifier
+    //Donc on va mettre verificationDeplacement ici
+}
