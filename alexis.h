23e51be2a5f897a4b1cba8cd91aa5b05021f93c6
alexismarieece <alexis.marie@edu.ece.fr>
2021-12-07 12:05:22 +0100

    Update alexis.h

--- alexis.h
+++ alexis.h
@@ -12,3 +12,5 @@ void deplacerPion(int i,int j,int i1,int j1,int matrice[17][17]);
 int verificationDeplacement(int i1,int j1,int matrice[17][17]);
 int verifierBarriere(int i,int j,int i1,int j1,int matrice[17][17]);
 int PionDansCase(int i1,int j1,int matrice[17][17]);
+
+int verifiePassage(int i, int j, int matrice[17][17]);
