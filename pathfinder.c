1a1350bba003c478836519b8e21ff6458d65e315
MarieRek <marie.rek01@gmail.com>
2021-12-10 00:34:50 +0100

    Implementation des méthodes de parcours du tableau

--- pathfinder.c
+++ pathfinder.c
@@ -2,6 +2,43 @@
 
 int CheckPath( int grid[23][23], int taille, t_joueur joueur, int indexJoueur)
 {
+    int verifGrid[taille][taille];
+    for (int lig=0; lig<taille; lig++)
+        {
+
+            for (int col=0; col<taille; col++)
+            {
+                verifGrid[lig][col] = 0;
+            }
+        }
+    int ligJoueur = joueur.coordonneeMatrice.ligne / 2;
+    int colJoueur = joueur.coordonneeMatrice.colonne / 2;
+    verifGrid[ligJoueur][colJoueur]=1;
+    int nbCelluleModif=0;
+    do
+    {
+        nbCelluleModif=0;
+        for (int lig=0; lig<taille; lig++)
+        {
+            int ligMatrice = lig * 2;
+            for (int col=0; col<taille; col++)
+            {
+                int colMatrice = col * 2;
+                if(verifGrid[lig][col] == 0)
+                {
+                    if((lig > 0 && verifGrid[lig - 1][col] == 1 && grid[ligMatrice - 1][colMatrice] == 0)
+                       || (col > 0 && verifGrid[lig][col - 1] == 1 && grid[ligMatrice][colMatrice - 1] == 0)
+                       || (lig < taille - 1 && verifGrid[lig + 1][col] == 1 && grid[ligMatrice + 1][colMatrice] == 0)
+                       || (col < taille - 1 && verifGrid[lig][col + 1] == 1 && grid[ligMatrice][colMatrice + 1] == 0))
+                    {
+                        verifGrid[lig][col] = 1;
+                        nbCelluleModif++;
+                    }
+                }
+            }
+        }
+    } while (nbCelluleModif > 0);
+
     return 0;
 }
 
